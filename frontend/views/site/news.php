7cf250ddf6aff7a2d7d647438324b6c9c13df610
lyj789 <lyj981014@163.com>
2018-07-30 22:13:36 +0800

    文章与数据库

--- frontend/views/site/news.php
+++ frontend/views/site/news.php
@@ -1,40 +1,179 @@
-<?php
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title></title>
+    <!-- Bootstrap core CSS -->
+    <link href="css_new/bootstrap.css" rel="stylesheet">
+    <!-- DL Menu CSS -->
+    <link href="js/dl-menu/component.css" rel="stylesheet">
+    <!-- Slick Slider CSS -->
+    <link href="css_new/slick.css" rel="stylesheet">
+    <link href="css/slick-theme.css" rel="stylesheet">
+    <!-- ICONS CSS -->
+    <link href="css_new/font-awesome.css" rel="stylesheet">
+    <link href="css/svg-icons.css" rel="stylesheet">
+    <!-- Pretty Photo CSS -->
+    <link href="css_new/prettyPhoto.css" rel="stylesheet">
+    <!-- Typography CSS -->
+    <link href="css_new/typography.css" rel="stylesheet">
+    <!-- Widget CSS -->
+    <link href="css_new/widget.css" rel="stylesheet">
+    <!-- Shortcodes CSS -->
+    <link href="css_new/shortcodes.css" rel="stylesheet">
+    <!-- Custom Main StyleSheet CSS -->
+    <link href="style.css" rel="stylesheet">
+    <!-- Color CSS -->
+    <link href="css_new/color.css" rel="stylesheet">
+    <!-- Responsive CSS -->
+    <link href="css_new/responsive.css" rel="stylesheet">
+ 
+  </head>
+
+  <?php
 
 use frontend\components\NewsBarWidget;
 use frontend\components\CutBarWidget;
+use yii\helpers\Html;
+use yii\helpers\URL;
+use frontend\models\Favorite;
+use frontend\models\Passage;
+use frontend\models\Comment;
 
-/* @var $this yii\web\View */
-
-$this->title = 'PSYCH-WorldCup';
+$this->title = '热门文章';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-index">    
-    <?= CutBarWidget::widget([
-        'date' => date_create("2018-07-25"),
-    ]) ?>
-    <?php echo NewsBarWidget::widget([
-        'title' => '2018世界杯“战报”:亚马逊上哪些国家级明星球员的球衣最热销?',
-        'imgUrl'=> '../web/static/news/20180723/cosmic.jpg',
-        'brief'=>'国际足联世界杯（FIFA）是世界上最高规格、最高竞技水平、最高知名度的足球
-                比赛，共有32支球队参加，进行64场比赛，分别在11个不同的主办城市的12座体育场进行。
-                根据国际足联的数据显示，2014年世界杯的收视率创下世界纪录，观看世界杯的观众超过32亿，
-                其中，有超过10亿观众观看德国和阿根廷之间的决赛。在美国，观看世界杯的人数一直在增长，
-                今年美国有望成为史上观看世界杯人数最多的国家。 ',
-    ]) ?> 
-    <?= CutBarWidget::widget([
-        'date' => date_create("2018-06-25"),
-    ]) ?>
-    <?php echo NewsBarWidget::widget([
-        'title' => 'title',
-        'imgUrl'=> '../web/static/news/20180723/cosmic.jpg',
-        'brief'=>'a brief',
-    ]) ?>
-    <?php echo NewsBarWidget::widget([
-        'title' => 'title',
-        'imgUrl' => '../web/static/news/20180723/cosmic.jpg',
-        'brief' => 'a brief',
-    ]) ?>
-    <!--下面需要一个分页
-    https://www.yiichina.com/tutorial/93?sort=desc
-    -->
 
+  <body>
+
+<!--kode Wrapper Start-->  
+<div class="kode_wrapper"> 
+
+    <!--Header 2 Wrap End-->
+    <!--Inner Banner Start-->
+  
+    <!--Inner Banner End-->
+    <div class="kode_content_wrap">
+        <section class="kf_blog2">
+            <div class="container">
+                <!--Featured 3 Slider Start-->
+                <div class="kf_featured_wrap3">
+                    <div class="row">
+
+
+                        <?php foreach($passage as $mat):?>
+                        <div class="col-md-6 col-sm-6">
+                            <!--Featured 3 thumb Start-->
+                            <div class="kf_featured_thumb">
+                                <figure>
+                                    
+                                     <img src=<?php echo URL::to('extra-images/feature_'.$mat->ID.'.jpg')?> alt="">
+
+                                </figure>
+
+                              
+
+                                <div class="text_wrper">
+                                    <div class="text">
+                                        <h6>FIFA worldcup 2018</h6>                              
+                                        <h2><?=Html::encode("$mat->title")?></h2>
+                                        <h6><?=Html::encode("$mat->date")?></h6>
+                                        <p><?=Html::encode("$mat->content")?> </p>
+                                        <a class="btn_2" href=<?php echo URL::to('./?r=passage%2Fview&id='.$mat->ID)?>>Read More</a>
+
+                                    </div>
+                                    <div class="thumb_footer">
+                                        <div class="admin_thumb">
+                                           
+                                            <h6><?=Html::encode("$mat->author")?></h6>
+                                        </div>
+                                        <ul class="blog_meta">
+                                            <li>
+                                                <span><i class="fa fa-eye"></i><?=Html::encode("$mat->seen")?></span>
+                                            </li>
+                                            <?php //$like=passage::find()->select(['$mat->ID']);
+                                             $amount=favorite::find()->where(["passage_ID" => $mat->ID])->count();?>
+                                            <li>
+                                                <span class="heart"><i class="fa fa-heart-o"></i> <?= Html::encode("$amount") ?>  </span>
+                                            </li>
+                                              <?php //$like=passage::find()->select(['$mat->ID']);
+                                             $amount=comment::find()->where(["passage_ID" => $mat->ID])->count();?>
+                                            <li>
+                                                <a href="#"><i class="fa fa-comment-o"></i><?= Html::encode("$amount") ?>  </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                        
+
+
+                             </div>
+                            <!--Featured 3 thumb End-->
+                        </div>
+                        <?php endforeach;?>
+
+
+
+                     
+                            <!--Featured 3 thumb End-->
+                        </div>
+                    </div>
+                </div>
+                <!--Featured 3 Slider End-->
+                <!--Kf Pagination Start-->
+                <div class="kode-pagination text-center">
+                    <span class="page-numbers current">1</span>
+                    <a class="page-numbers" href="#">2</a>
+                    <a class="page-numbers" href="#">3</a>
+                    <a class="page-numbers border_none" href="#">...</a>
+                    <a class="page-numbers" href="#">18</a>
+                    <a class="page-numbers" href="#">19</a>
+                    <a class="page-numbers" href="#">20</a>
+                </div>
+                <!--Kf Pagination End-->
+            </div>
+        </section>
+    </div>
+    <!--Main Content Wrap End-->
+    <!--ticker Wrap Start-->
+   
+    <!--ticker Wrap End-->
+    <!--Footer Wrap Start-->
+   
+    <!--Footer Wrap End-->
+    <!--Copy Right Wrap Start-->
+   
+    <!--Copy Right Wrap End-->
+    <!--Register Pop Up Wrap Start-->
+    <div class="modal fade" id="search" tabindex="-1" role="dialog" aria-labelledby="search">
+        <div class="modal-dialog" role="document">
+            <div class="input_dec">
+                <input type="text" placeholder="search......">
+                <button class="btn_icon"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+    </div>        
+    <!--Register Pop Up Wrap End-->
 </div>
+<!--kode Wrapper End-->
+    <!--Jquery Library-->
+    <script src="js_new/jquery.js"></script>
+    <!--Bootstrap core JavaScript-->
+    <script src="js_new/bootstrap.js"></script>
+    <!--Slick Slider JavaScript-->
+    <script src="js_new/slick.min.js"></script>
+    <!--Pretty Photo JavaScript-->
+    <script src="js_new/jquery.prettyPhoto.js"></script>
+    <!--Dl Menu Script-->
+    <script src="js_new/dl-menu/modernizr.custom.js"></script>
+    <script src="js_new/dl-menu/jquery.dlmenu.js"></script>
+    <!--Custom JavaScript-->
+    <script src="js_new/custom.js"></script>
+
+  </body>
+</html>
+
+
