52f5566fa7fa5615c84f15c9905606c285206530
rmdyh <rmdyh@hotmail.com>
2018-08-03 01:30:12 +0800

    优化用户后台界面

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -25,31 +25,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create User'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'username',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
-            'email:email',
-            'status',
-            'created_at',
-            'updated_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
 </div>
 
-<div class="admin-userspan" id="admin-userspan" style="padding:100px 40px; background: white">
+<div class="admin-userspan" id="admin-userspan" style="padding:10px 40px; ">
 
     <p class="text-center">
         User Admin
